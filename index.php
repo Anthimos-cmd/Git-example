b0f21595eaf032bf9d348cb92a74e69c020c1926
anthimos <anthimosisk@gmail.com>
2020-03-20 20:24:01 +0200

    My third code lines

--- index.php
+++ index.php
@@ -9,6 +9,21 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed:" . $conn->connect_error);
+
 }
 $sql = "SELECT sch_id, sch_name, sch_long, sch_lat FROM schools";
+$result + $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    //output data of each row
+    while($row = $result->fetch_assoc()){
+        echo "scool id: " .$row["sch_id"]. "- Name: " .$row["sch_iname"]. "- long: " . $row["sch_long"]. "- lat: " .$row["sch_lat"]." <br>";
+}
+}
+else{
+    echo "0 results";
+}
+
+
+$conn->close();
 ?>
